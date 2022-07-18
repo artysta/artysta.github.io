eecc5216edf36c9ee5ddc4583346ab291e26c032
Adrian Kurek <adrian.kurek93@gmail.com>
2022-07-18 18:40:49 +0200

    Add a function that renders the workplaces section

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ window.onload = init;
 var isDarkModeOn = true;
 
 function init() {
+    renderWorkplaces();
     setLastEditedAtDate();
 }
 
@@ -54,4 +55,55 @@ function setLastEditedAtDate() {
         footer.innerHTML = `Last edited at ${lastEditedAtDate} UTC | <a href="${latestCommit.html_url}" target="_blank">${latestCommit.sha}</a>`;
     })
     .catch(error => footer.innerHTML = `Last edited at N/A`);
+}
+
+function renderWorkplaces() {
+    let workplacesContainer = document.getElementById("workplaces");
+    fetch("resume.json")
+        .then(response => response.json())
+        .then(resume => {
+            let sectionTitle = document.createElement("h2");
+            sectionTitle.innerHTML = resume.workplaces.title;
+            workplacesContainer.appendChild(sectionTitle);
+
+            resume.workplaces.items.forEach(workplace => {
+                let divGroupContainer = document.createElement("div");
+                divGroupContainer.classList.add("group-container");
+
+                let divLogoContainer = document.createElement("div");
+                divLogoContainer.classList.add("group-logo-container");
+
+                let img = document.createElement("img");
+                img.src = workplace.logoUrl;
+
+                let divPositionsContainer = document.createElement("div");
+
+                workplace.positions.forEach(position => {
+                    // POSITION START
+                    let positionUl = document.createElement("ul");
+                    let positionLi = document.createElement("li");
+                    positionLi.innerHTML = `<strong>${position.dateFrom} - ${position.dateThru}</strong> - ${position.title}`;
+                    positionUl.appendChild(positionLi);
+
+                    position.duties.forEach(duty => {
+                        // DUTY START
+                        let dutyUl = document.createElement("ul");
+                        let dutyLi = document.createElement("li");
+                        dutyLi.innerHTML = duty.value;
+                        dutyUl.appendChild(dutyLi);
+                        positionUl.appendChild(dutyUl);
+                        // DUTY END
+                    });
+                    // POSITION END
+                    divPositionsContainer.appendChild(positionUl);
+                });
+
+                divLogoContainer.appendChild(img);
+                divGroupContainer.appendChild(divLogoContainer);
+                divGroupContainer.appendChild(divPositionsContainer);
+                workplacesContainer.appendChild(divGroupContainer);
+            });
+        });
+
+    workplaces.appendChild(ulPosition);
 }
