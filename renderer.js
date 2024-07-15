15d4d2496a410cbe523d3c1608b8cefee5889902
Adrian Kurek <adrian.kurek93@gmail.com>
2024-07-15 15:47:42 +0200

    Revert "Refactor"
    This reverts commit 4ca9b3a36df5935b523f03acfb4f9116313fbaa5.

--- renderer.js
+++ renderer.js
@@ -32,16 +32,16 @@ const renderLine = (color) => `
 `;
 
 const renderFooterTemplate = (commits, color) => {
-    if (commits[0]) {
-        return `
+        if (commits[0] != undefined) {
+            return `
             ${renderLine(color)}
             <div class="footer">
-                Last edited at ${commits[0].commit.committer.date.replace('T', ' ').replace('Z', '')} UTC | 
-                <a href="${commits[0].html_url}" target="_blank" class="url-${color}-dark">${commits[0].sha}</a>
+                ${`Last edited at ${commits[0].commit.committer.date.replace('T', ' ').replace('Z', '')} UTC | <a href="${commits[0].html_url}" target="_blank" class="url-${color}-dark">${commits[0].sha}</a>`}
             </div>
-        `;
+        `
     }
-    return '';
+
+    return ``;
 }
 
 const renderButtonTemplate = (color, text) => `
@@ -64,10 +64,10 @@ const renderSection = (wrapper, section, color) => {
         section.items.forEach(item => {
             const li = document.createElement('li');
             li.innerHTML = `
-                ${item.url ? `<a href="${item.url}" target="_blank" class="url-${color}-dark">${item.title}</a>` : item.title}
+                ${item.url !== '' ? `<a href="${item.url}" target="_blank" class="url-${color}-dark">${item.title}</a>` : item.title}
                 <ul>
-                    ${item.items2 ? item.items2.map(item2 => `
-                        <li>${item2.url ? `<a href="${item2.url}" target="_blank" class="url-${color}-dark">${item2.value}</a>` : item2.value}</li>
+                    ${(item.items2 && item.items2.length > 0) ? item.items2.map(item2 => `
+                        <li>${item2.url !== '' ? `<a href="${item2.url}" target="_blank" class="url-${color}-dark">${item2.value}</a>` : item2.value}</li>
                     `).join('') : ''}
                 </ul>
             `;
@@ -76,56 +76,57 @@ const renderSection = (wrapper, section, color) => {
     }
 
     if (section.hasOwnProperty('itemsWithLogo')) {
-        mainDiv.insertAdjacentHTML('afterend', renderItemsWithLogoTemplate(section));
+        section.itemsWithLogo.forEach(item => {
+            mainDiv.insertAdjacentHTML('afterend', renderItemsWithLogoTemplate(section))
 
-        const img = document.getElementById(`logo-${section.key}`);
-        img.src = section.itemsWithLogo[0].logoUrl;
-        img.dataset.name = section.itemsWithLogo[0].dataName;
-        img.dataset.extension = section.itemsWithLogo[0].logoUrl.split('.').pop();
+            const img = document.getElementById(`logo-${section.key}`);
+            const logoUrlParts = item.logoUrl.split('.');
+            img.src = item.logoUrl;
+            img.dataset.name = item.dataName;
+            img.dataset.extension = logoUrlParts[logoUrlParts.length - 1];
 
-        const ulOutter = document.getElementById(`ul-detailed-${section.key}`);
-        const title = document.createElement('h3');
-        title.textContent = section.itemsWithLogo[0].name;
-        ulOutter.prepend(title);
+            const ulOutter = document.getElementById(`ul-detailed-${section.key}`);
+            const title = document.createElement('h3');
+            title.textContent = item.name;
+            ulOutter.prepend(title);
 
-        section.itemsWithLogo[0].positions.forEach(position => {
-            const liOutter = document.createElement('li');
-            const ulInner = document.createElement('ul');
-            liOutter.innerHTML = `<strong>${position.dateFrom} - ${position.dateThru}</strong> - ${position.title}`;
-
-            if (!position.isActive) {
-                liOutter.innerHTML = `<s>${liOutter.innerHTML}</s>`;
-            }
-
-            ulOutter.appendChild(liOutter);
-
-            position.details.forEach(detail => {
-                const liInner = document.createElement('li');
-                liInner.innerHTML = detail.value;
+            item.positions.forEach(position => {
+                const liOutter = document.createElement('li');
+                const ulInner = document.createElement('ul');
+                liOutter.innerHTML = `<strong>${position.dateFrom} - ${position.dateThru}</strong> - ${position.title}`;;
 
                 if (!position.isActive) {
-                    liInner.innerHTML = `<s>${liInner.innerHTML}</s>`;
+                    liOutter.innerHTML = `<s>${liOutter.innerHTML}</s>`;
                 }
 
-                ulInner.appendChild(liInner);
-                liOutter.appendChild(ulInner);
+                ulOutter.appendChild(liOutter);
+
+                position.details.forEach(detail => {
+                    const liInner = document.createElement('li');
+                    liInner.innerHTML = detail.value;
+
+                    if (!position.isActive) {
+                        liInner.innerHTML = `<s>${liInner.innerHTML}</s>`;
+                    }
+
+                    ulInner.appendChild(liInner);
+                    liOutter.appendChild(ulInner);
+                });
             });
         });
     }
 
     if (section.hasOwnProperty('icons')) {
-        mainDiv.insertAdjacentHTML('afterend', renderIconsTemplate(section));
+        mainDiv.insertAdjacentHTML('afterend', renderIconsTemplate(section))
         const divIconsContainer = document.getElementById(`div-icons-${section.key}`);
 
         section.icons.forEach(icon => {
             const div = document.createElement('div');
-            div.classList.add('div-image');
+            div.classList = 'div-image';
 
             const i = document.createElement('i');
-            i.classList.add(`icon-${color}-dark`, ...icon.class.split(' '));
-            if (icon.class.split(' ').length <= 2) {
-                i.style.fontSize = icon.size;
-            }
+            i.classList = `icon-${color}-dark ${icon.class}`;
+            i.style = `font-size:${icon.class.split(' ').length <= 2 ? icon.size : ''}`;
 
             const p = document.createElement('p');
             p.innerHTML = icon.name;
@@ -144,4 +145,4 @@ export {
     renderButtonTemplate,
     renderMessageTemplate,
     renderSection
-};
+}
